d3363d10d1567d476ac10b1e38faf5717c40da95
lgs0503 <tjrdl0503@naver.com>
2023-04-11 20:12:24 +0900

    Spring API 문서 자동 정리 Swagger 설정

--- src/main/java/com/gs/bbs/sample/controller/SampleController.java
+++ src/main/java/com/gs/bbs/sample/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.gs.bbs.sample.controller;
 
 import com.gs.bbs.sample.service.SampleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+
 @RestController
 @RequestMapping(value = "/sample")
+@Tag(name = "Sample", description = "샘플 api")
 public class SampleController {
 
     @Autowired
     SampleService sampleService;
 
+    @Operation(summary = "샘플조회", description = "샘플조회 메서드입니다.")
     @GetMapping
     public Map<String, Object> getSample(){
 
