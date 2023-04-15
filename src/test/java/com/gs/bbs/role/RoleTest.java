b3912544319ee444d4d1deb5250be38fc6b3bd62
lgs0503 <tjrdl0503@naver.com>
2023-04-15 22:48:52 +0900

    권한 Junit Test 어노테이션 누락 및 테스트 소스 수정

--- src/test/java/com/gs/bbs/role/RoleTest.java
+++ src/test/java/com/gs/bbs/role/RoleTest.java
@@ -4,6 +4,7 @@ import com.gs.bbs.api.role.dto.RoleDTO;
 import com.gs.bbs.api.role.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,6 +18,7 @@ public class RoleTest {
     @Autowired
     RoleService roleService;
 
+    @Test
     @DisplayName("권한 리스트 조회")
     public void getRoleList() {
 
@@ -30,17 +32,19 @@ public class RoleTest {
         }
     }
 
+    @Test
     @DisplayName("권한 조회")
     public void getRole() {
         RoleDTO roleDTO = roleService.getRole(1);
         log.debug(roleDTO.toString());
     }
 
+    @Test
     @DisplayName("권한 추가")
     public void insertRole(){
 
         RoleDTO roleDTO = new RoleDTO();
-        roleDTO.setName("ADMIN");
+        roleDTO.setName("TESTER");
         roleDTO.setCreateUser("admin");
 
         int result = roleService.insertRole(roleDTO);
@@ -49,32 +53,32 @@ public class RoleTest {
         getRoleList();
     }
 
+    @Test
     @DisplayName("권한 수정")
     public void updateRole(){
 
         RoleDTO roleDTO = new RoleDTO();
-        roleDTO.setRoleId(1);
-        roleDTO.setName("ADMIN Test");
+        roleDTO.setRoleId(3);
+        roleDTO.setName("TESTER_UPDATE");
         roleDTO.setUpdateUser("admin");
-        roleDTO.setUseYn("Y");
 
-        int result = roleService.updateRole(null);
+        int result = roleService.updateRole(roleDTO);
         log.debug("updateRole Result : " + result);
 
         getRoleList();
     }
 
+    @Test
     @DisplayName("권한 삭제")
     public void deleteRole(){
 
         List<Integer> roleIds = new ArrayList<Integer>();
 
-        roleIds.add(1);
+        roleIds.add(3);
 
         int result = roleService.deleteRole(roleIds);
         log.debug("deleteRole Result : " + result);
 
         getRoleList();
     }
-
 }
