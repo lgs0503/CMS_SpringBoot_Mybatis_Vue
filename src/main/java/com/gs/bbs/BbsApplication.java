1188115e4f321ac10f9f3fbd55fab90a510415d1
lgs0503 <tjrdl0503@naver.com>
2023-04-16 09:06:50 +0900

    Spring Security 기본 로그임폼 사용안함

--- src/main/java/com/gs/bbs/BbsApplication.java
+++ src/main/java/com/gs/bbs/BbsApplication.java
@@ -2,10 +2,11 @@ package com.gs.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BbsApplication {
 
     public static void main(String[] args) {
