f76846a49aeaee7bef9edbada92a21c0b9e792b1
lgs0503 <tjrdl0503@naver.com>
2023-04-22 12:58:01 +0900

    JWT 토큰 발행 추가

--- src/main/java/com/gs/bbs/config/SecurityConfig.java
+++ src/main/java/com/gs/bbs/config/SecurityConfig.java
@@ -1,65 +1,29 @@
 package com.gs.bbs.config;
 
-import com.gs.bbs.jwt.JwtAccessDeniedHandler;
-import com.gs.bbs.jwt.JwtAuthenticationEntryPoint;
-import com.gs.bbs.jwt.TokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final TokenProvider tokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAtuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return (web) -> web.ignoring()
-                .antMatchers("/favicon.ico");
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .csrf().disable()
-
-                /**401, 403 Exception 핸들링 */
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAtuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-
-                /**세션 사용하지 않음*/
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                /** HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정*/
-                .and()
-                .authorizeRequests()
-                .antMatchers("/user/**").permitAll()
-
-                /**JwtSecurityConfig 적용 */
-                .and()
-                .apply(new JwtSecurityConfig(tokenProvider))
-
-                .and().build();
+                .formLogin();
     }
 }
