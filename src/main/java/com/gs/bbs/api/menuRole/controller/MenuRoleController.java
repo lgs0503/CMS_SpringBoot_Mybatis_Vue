67f27df5630ea413f54642842e882d4d01933006
lgs0503 <tjrdl0503@naver.com>
2023-04-23 19:19:33 +0900

    메뉴 별 권한 RequestMapping 누락

--- src/main/java/com/gs/bbs/api/menuRole/controller/MenuRoleController.java
+++ src/main/java/com/gs/bbs/api/menuRole/controller/MenuRoleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/menuRole")
 @Slf4j
 @Tag(name = "Menu Role", description = "메뉴 권한 api")
 public class MenuRoleController {
