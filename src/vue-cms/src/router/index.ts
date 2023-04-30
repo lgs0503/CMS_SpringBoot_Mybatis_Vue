11dd8fd5b93267454abb2302ae5e9d84a749329e
lgs0503 <tjrdl0503@naver.com>
2023-04-30 12:29:46 +0900

    로그인 여부 리다이렉트 라우터 수정

--- src/vue-cms/src/router/index.ts
+++ src/vue-cms/src/router/index.ts
@@ -6,12 +6,20 @@ import AdminHomeView from '../views/admin/home/AdminHomeView.vue'
 import AdminCodeMgmtView from '../views/admin/code/AdminCodeMgmtView.vue'
 
 const loginTokenCheck = () : string => {
-  
-  // 토큰 세션체크
 
-  return "/admin/home";
+  const token = sessionStorage.getItem("loginToken");
+  console.log(token);
+
+  let redirectURL = "";
+
+  if (token === null) {
+    redirectURL = "/admin/login";
+  } else {
+    redirectURL = "/admin/home";
+  }
+
+  return redirectURL;
 }
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,14 +28,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/admin/login',
