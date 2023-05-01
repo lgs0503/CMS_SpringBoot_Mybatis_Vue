f4f3a46ae5eaaab2160df85a3e15d75cfefacc21
lgs0503 <tjrdl0503@naver.com>
2023-05-01 10:04:00 +0900

    공통 알림 확인창 app.vue로 이동

--- src/vue-cms/src/App.vue
+++ src/vue-cms/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AlertConfirm from "@/components/common/AlertConfirm.vue";
 </script>
 
 <template>
   <RouterView />
+  <AlertConfirm/>
 </template>
 
 <style scoped>
