ef171d0d790b9f926aafbc2ef603bf5a65f41982
lgs0503 <tjrdl0503@naver.com>
2023-05-01 10:04:52 +0900

    공통 alert confirm 추가

---
+++ src/vue-cms/src/components/common/AlertConfirm.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import {useAlertConfirmStore} from "@/stores/common/alertConfirmStore";
+  const alertConfirm = useAlertConfirmStore();
+
+  const alertConfirmOk = () => {
+
+    if (alertConfirm.callback) {
+      alertConfirm.callback();
+    }
+
+    alertConfirm.isHidden = false;
+  }
+
+  const confirmCancel = () => {
+    alertConfirm.isHidden = false;
+  }
+</script>
+
+<template>
+  <div v-if="alertConfirm.isHidden" class="commonAlertConfirm">
+    <div class="header">
+      {{ alertConfirm.title }}
+    </div>
+    <div class="body">
+      {{ alertConfirm.message }}
+    </div>
+    <div class="footer">
+      <button @click="alertConfirmOk">확인</button>
+      <button v-if="alertConfirm.isConfirm" @click="confirmCancel">취소</button>
+    </div>
+  </div>
+  <div class="commonAlertConfirm-Background"></div>
+</template>
+<style>
+</style>
