edb12b1adb24e8bfef993b5e082c86c8c43ad711
lgs0503 <tjrdl0503@naver.com>
2023-05-02 18:10:33 +0900

    공통 알림창 scss 추가

--- src/vue-cms/src/components/common/AlertConfirm.vue
+++ src/vue-cms/src/components/common/AlertConfirm.vue
@@ -29,7 +29,62 @@
       <button v-if="alertConfirm.isConfirm" @click="confirmCancel">취소</button>
     </div>
   </div>
-  <div class="commonAlertConfirm-Background"></div>
+  <div v-if="alertConfirm.isHidden" class="commonAlertConfirm-Background"></div>
 </template>
-<style>
+<style lang="scss" scoped>
+  @import "../src/assets/variables.scss";
+
+  .commonAlertConfirm {
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%, -50%);
+    border:none;
+    display: inline-block;
+    z-index: 5001;
+    border-radius: 5px;
+    background: white;
+
+    .header {
+      background-color: #{$blue-dark-color};
+      color: #{$white-color};
+      padding: 10px 20px;
+      border-radius: 5px 5px 0 0;
+    }
+
+    .body {
+      padding: 20px;
+      min-width: 200px;
+      color: #{$sliver-dark-color};
+      font-size: 12px;
+      font-weight: bold;
+    }
+
+    .footer {
+      padding: 10px;
+      text-align: right;
+
+      button {
+        background-color: #{$blue-dark-color};
+        color: #{$white-color};
+        border: none;
+        padding: 5px 20px;
+        border-radius: 3px;
+        margin-left:5px;
+      }
+
+      button:hover{
+        cursor: pointer;
+        background-color: #{$blue-deep-dark-color};
+      }
+    }
+  }
+
+  .commonAlertConfirm-Background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.6);
+    z-index: 5000;
+  }
 </style>
