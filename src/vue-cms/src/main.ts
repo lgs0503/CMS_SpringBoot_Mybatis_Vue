cf3c894bec5f7936460c0c20540374d0e98b1c5c
lgs0503 <tjrdl0503@naver.com>
2023-05-03 17:55:55 +0900

    font-awesome 의존성  추가 및 기본설정

--- src/vue-cms/src/main.ts
+++ src/vue-cms/src/main.ts
@@ -5,8 +5,17 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/reset.css'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 const app = createApp(App)
+
+library.add(fab, far, fas)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
