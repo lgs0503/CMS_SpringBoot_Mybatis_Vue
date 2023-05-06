4f10742c9cea313cd08daae257fe487879998bc6
lgs0503 <tjrdl0503@naver.com>
2023-05-06 09:06:51 +0900

    메뉴 타이틀 컴포넌트 추가

---
+++ src/vue-cms/src/components/common/MenuTitle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+  defineProps({
+    title:{
+      Type: String,
+      default : ""
+    }
+  });
+
+</script>
+
+<template>
+  <div class="admin-content-title">{{title}}</div>
+</template>
+<style lang="scss" scoped>
+  @import "../src/assets/variables.scss";
+
+  .admin-content-title {
+    font-size: 20px;
+    font-weight: bold;
+    border-left: double 10px #{$blue-dark-color};
+    padding-left: 10px;
+    height: 28px;
+    line-height: 25px;
+    color: #{$sliver-dark-color};
+  }
+</style>
