74a2efbf1a011c8ae4146f6263689f25489e1160
lgs0503 <tjrdl0503@naver.com>
2023-05-20 20:00:25 +0900

    CORS PUT DELETE PATCH 안되는 버그 수정

--- src/main/java/com/gs/bbs/config/CorsConfig.java
+++ src/main/java/com/gs/bbs/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.gs.bbs.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173");
+                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 }
