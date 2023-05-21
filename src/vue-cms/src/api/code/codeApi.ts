5cb08064ea51fc4aec8d7cb4b31423d7f0201656
lgs0503 <tjrdl0503@naver.com>
2023-05-21 10:24:12 +0900

    코드관리 api store 작성

--- src/vue-cms/src/api/code/codeApi.ts
+++ src/vue-cms/src/api/code/codeApi.ts
@@ -1,4 +1,5 @@
 import axiosInstance from "@/api/axiosInit";
+import type {CodeModel, GroupCodeModel} from "@/model/code/codeModel";
 
 export const GET_CODE_LIST = async (
     codeId?: number,
@@ -8,12 +9,16 @@ export const GET_CODE_LIST = async (
     useYn?: string
 ) => {
      return await axiosInstance.get(
-         `/code?
-          codeId=${codeId ? codeId: "0"}&
-          groupCodeId=${groupCodeId ? groupCodeId : "0"}&
-          groupCodeName=${groupCodeName ? groupCodeName : ""}&
-          name=${name ? name : ""}&
-          userYn=${useYn ? useYn : "Y"}`
+         "/code",
+         {
+               params: {
+                    codeId: codeId,
+                    groupCodeId: groupCodeId,
+                    groupCodeName: groupCodeName,
+                    name: name,
+                    useYn: useYn
+               }
+         }
      );
 }
 
@@ -22,6 +27,72 @@ export const GET_GROUP_CODE_LIST_KEYWORD = async (
     keyword?: string
 ) => {
      return await axiosInstance.get(
-         `/groupCode/keyword?searchKey=${searchKey ? searchKey : ""}&keyword=${keyword ? keyword : ""}`
+         "/groupCode/keyword",
+         {
+             params: {
+                 searchKey: searchKey,
+                 keyword: keyword
+             }
+         }
      );
+}
+
+export const GET_GROUP_CODE = async (
+    groupCodeId?: number
+) => {
+    return await axiosInstance.get(`/groupCode/${groupCodeId}`);
+}
+
+export const GET_CODE = async (
+    codeId?: number
+) => {
+    return await axiosInstance.get(`/code/${codeId}`);
+}
+
+export const INSERT_GROUP_CODE = async (
+    groupCodeModel?: GroupCodeModel
+) => {
+    return await axiosInstance.post("/groupCode", groupCodeModel);
+}
+
+export const INSERT_CODE = async (
+    codeModel?: CodeModel
+) => {
+    return await axiosInstance.post("/code", codeModel);
+}
+
+export const UPDATE_GROUP_CODE = async (
+    groupCodeModel?: GroupCodeModel
+) => {
+    return await axiosInstance.put("/groupCode", groupCodeModel);
+}
+
+export const UPDATE_CODE = async (
+    codeModel?: CodeModel
+) => {
+    return await axiosInstance.put("/code", codeModel);
+}
+
+export const DELETE_GROUP_CODE = async (
+    groupCodeIds?: Array<number>
+) => {
+    return await axiosInstance.delete("/groupCode", {
+        data: groupCodeIds
+    });
+}
+
+export const DELETE_CODE = async (
+    codeIds?: Array<number>
+) => {
+    return await axiosInstance.delete("/code", {
+        data: codeIds
+    });
+}
+
+export const DELETE_CODE_GROUP_CODE_ID = async (
+    groupCodeId?: number
+) => {
+    return await axiosInstance.delete("/code/groupCodeId", {
+        data: groupCodeId
+    });
 }
--- src/vue-cms/src/stores/code/codeStore.ts
+++ src/vue-cms/src/stores/code/codeStore.ts
@@ -1,24 +1,85 @@
 import { defineStore } from 'pinia'
-import {GET_CODE_LIST, GET_GROUP_CODE_LIST_KEYWORD} from "@/api/code/codeApi";
+import {
+  GET_CODE,
+  GET_CODE_LIST,
+  GET_GROUP_CODE,
+  GET_GROUP_CODE_LIST_KEYWORD,
+  INSERT_CODE,
+  INSERT_GROUP_CODE,
+  UPDATE_CODE,
+  UPDATE_GROUP_CODE,
+  DELETE_CODE,
+  DELETE_GROUP_CODE,
+  DELETE_CODE_GROUP_CODE_ID
+} from "@/api/code/codeApi";
+import type {CodeModel, GroupCodeModel} from "@/model/code/codeModel";
 
 export const useCodeStore = defineStore('code', () => {
 
   const getCodeList = async (
-      codeId?: number,
-      groupCodeId?: number,
-      groupCodName?: string,
-      name?: string,
-      useYn?: string
+      codeId?:number,
+      groupCodeId?:number,
+      groupCodName?:string,
+      name?:string,
+      useYn?:string
   ) => {
     return await GET_CODE_LIST(codeId, groupCodeId, groupCodName, name, useYn);
   }
 
   const getGroupCodeListKeyword = async (
-      searchKey?: string,
-      keyword?: string,
+      searchKey?:string,
+      keyword?:string,
   ) => {
     return await GET_GROUP_CODE_LIST_KEYWORD(searchKey, keyword);
   }
 
-  return { getCodeList, getGroupCodeListKeyword }
+  const getGroupCode = async (groupCodeId?:number) => {
+    return await GET_GROUP_CODE(groupCodeId);
+  }
+
+  const getCode = async (codeId?:number) => {
+    return await GET_CODE(codeId);
+  }
+
+  const insertGroupCode = async (groupCodeModel?:GroupCodeModel) => {
+    return await INSERT_GROUP_CODE(groupCodeModel);
+  }
+
+  const insertCode = async (codeModel?:CodeModel) => {
+    return await INSERT_CODE(codeModel);
+  }
+
+  const updateGroupCode = async (groupCodeModel?:GroupCodeModel) => {
+    return await UPDATE_GROUP_CODE(groupCodeModel);
+  }
+
+  const updateCode = async (codeModel?:CodeModel) => {
+    return await UPDATE_CODE(codeModel);
+  }
+
+  const deleteGroupCode = async (groupCodeIds?:Array<number>) => {
+    return await DELETE_GROUP_CODE(groupCodeIds);
+  }
+
+  const deleteCode = async (codeIds?:Array<number>) => {
+    return await DELETE_CODE(codeIds);
+  }
+
+  const deleteCodeGroupCodeId = async (groupCodeId?:number) => {
+    return await DELETE_CODE_GROUP_CODE_ID(groupCodeId);
+  }
+
+  return {
+    getCodeList,
+    getGroupCodeListKeyword,
+    getGroupCode,
+    getCode,
+    insertGroupCode,
+    insertCode,
+    updateGroupCode,
+    updateCode,
+    deleteGroupCode,
+    deleteCode,
+    deleteCodeGroupCodeId
+  }
 })
