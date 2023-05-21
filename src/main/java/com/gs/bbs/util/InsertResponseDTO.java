6baf27a745ece0ec761cc5b21517742e7df64aec
lgs0503 <tjrdl0503@naver.com>
2023-05-21 10:20:19 +0900

    insertDTO 추가 추가된 key값 들고오는용도

---
+++ src/main/java/com/gs/bbs/util/InsertResponseDTO.java
@@ -0,0 +1,13 @@
+package com.gs.bbs.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InsertResponseDTO {
+
+    private int result;
+    private int insertKey;
+
+}
