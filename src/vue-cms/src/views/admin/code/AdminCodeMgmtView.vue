b73477679b86d38c46cb0a31ac762c5aa704cf75
lgs0503 <tjrdl0503@naver.com>
2023-05-21 10:26:03 +0900

    코드관리 코드관리상세 화면추가

--- src/vue-cms/src/views/admin/code/AdminCodeMgmtView.vue
+++ src/vue-cms/src/views/admin/code/AdminCodeMgmtView.vue
@@ -1,98 +1,80 @@
 <script setup lang="ts">
 
-import MenuTitle from "@/components/common/MenuTitle.vue";
-import SearchForm from "@/components/common/SearchForm.vue";
-import CommonTable from "@/components/common/CommonTable.vue";
+  import MenuTitle from "@/components/common/MenuTitle.vue";
+  import SearchForm from "@/components/common/SearchForm.vue";
 
-import {onMounted, ref} from "vue";
-import {useCodeStore} from "@/stores/code/codeStore";
+  import {onMounted, ref} from "vue";
+  import {useCodeStore} from "@/stores/code/codeStore";
+  import CommonTable from "@/components/common/CommonTable.vue";
+  import router from "@/router";
+  import {API_RESULT_STATUS, VIEW_MODE} from "@/enum/enum";
+  import type { Ref } from 'vue'
+  import type {GroupCodeModel} from "@/model/code/codeModel";
 
-const searchKey = ref("");
-const keyword = ref("");
-const groupCodeKey = ref("groupCodeId");
-const groupCodeList = ref([]);
-const groupCodeListColumn = ref([
-  {
-    field : "groupCodeId",
-    name: "ID",
-    visible: true,
-    width: 30
-  },
-  {
-    field : "name",
-    name: "그룹코드명",
-    visible: true,
-    width: 100
-  },
-  {
-    field : "createUser",
-    name: "등록자",
-    visible: true,
-    width: 100
+  const searchKey:Ref<string> = ref("");
+  const keyword:Ref<string> = ref("");
+  const groupCodeList:Ref<Array<GroupCodeModel>> = ref([]);
+
+  const code = useCodeStore();
+
+  const groupCodeCol:Ref<Array<Object>> = ref([
+    {
+      name: "그룹코드 ID",
+      field: "groupCodeId",
+      width: "10%",
+      visible: true
+    },
+    {
+      name: "그룹코드명",
+      field: "name",
+      width: "30%",
+      visible: true
+    },
+    {
+      name: "그룹코드명(한글)",
+      field: "korName",
+      width: "30%",
+      visible: true
+    },
+    {
+      name: "등록자",
+      field: "createUser",
+      width: "10%",
+      visible: true
+    },
+    {
+      name: "등록일",
+      field: "createDate",
+      width: "20%",
+      visible: true
+    }
+  ]);
+
+  const searchEvent = async () => {
+
+    const result = await code.getGroupCodeListKeyword(searchKey.value, keyword.value);
+
+    if (result.status === API_RESULT_STATUS.SUCCESS) {
+      groupCodeList.value = result.data;
+    }
   }
-]);
 
-const codeList = ref([]);
-const codeListColumn = ref([
-  {
-    field : "codeId",
-    name: "코드 ID",
-    visible: true,
-    width: 30
-  },
-  {
-    field : "name",
-    name: "코드명",
-    visible: true,
-    width: 100
-  },
-  {
-    field : "groupCodeId",
-    name: "그룹코드 ID",
-    visible: true,
-    width: 30
-  },
-  {
-    field : "groupCodeName",
-    name: "그룹코드 명",
-    visible: true,
-    width: 100
-  },
-  {
-    field : "createUser",
-    name: "등록자",
-    visible: true,
-    width: 100
+  const groupCodeRowClickEvent = (groupCodeId:number) => {
+    router.push(`/admin/code/${groupCodeId}`);
   }
-]);
 
-const code = useCodeStore();
-
-const searchEvent = async () => {
-
-  const result = await code.getGroupCodeListKeyword(searchKey.value, keyword.value);
-
-  if (result.status === 200) {
-    groupCodeList.value = result.data;
+  const groupCodeAddView = () => {
+    router.push(`/admin/code/${VIEW_MODE.ADD_MODE}`);
   }
-}
 
-const rowClickEvent = async (value:number) => {
+  onMounted(async () => {
+    await searchEvent();
+  });
 
-  const result = await code.getCodeList(value.indexKey);
-
-  if (result.status === 200) {
-    codeList.value = result.data;
-  }
-}
-
-onMounted(async () => {
-  await searchEvent();
-});
 </script>
 <template>
   <div class="from">
-    <menu-title :title="`코드관리`"/>
+    <menu-title :title="`그룹코드관리`"/>
     <search-form
         class="search-form"
         v-model:comboBoxModel="searchKey"
@@ -100,37 +82,19 @@ onMounted(async () => {
         :groupCodeName="`CODE_SEARCH_OPTION`"
         @searchEvent="searchEvent"
     />
+    <div class="button-from">
+      <button class="btn-primary" @click="groupCodeAddView">추가</button>
+    </div>
     <div class="table-form">
-      <div class="table-master">
-        <div>
-          <div>그룹코드</div>
-          <div>
-            <button>추가</button>
-            <button>삭제</button>
-          </div>
-        </div>
-        <common-table
-            :column="groupCodeListColumn"
-            :data="groupCodeList"
-            :main-key="groupCodeKey"
-            @rowClickEvent="rowClickEvent"
-        />
-      </div>
-      <div class="table-detail">
-        <div>
-          <div>코드</div>
-          <div>
-            <button>추가</button>
-            <button>삭제</button>
-          </div>
-        </div>
-        <common-table
-            :column="codeListColumn"
-            :data="codeList"
-        />
-      </div>
+      <common-table
+          :column="groupCodeCol"
+          :data="groupCodeList"
+          @rowClickEvent="groupCodeRowClickEvent"
+          :mainKey="`groupCodeId`"
+      />
     </div>
   </div>
+
 </template>
 <style lang="scss" scoped>
 @import "../src/assets/variables.scss";
@@ -143,16 +107,29 @@ onMounted(async () => {
   .search-form{
     margin-top: 20px;
   }
-  .table-form {
-    display: flex;
-    margin-top: 20px;
 
-    .table-master{
-      width: calc( 30% - 5px );
-      margin-right: 10px;
+  .table-form {
+    margin-top: 10px;
+    width: 100%;
+  }
+
+  .button-from {
+    margin-top: 10px;
+    text-align: right;
+
+    .btn-primary {
+      font-weight: bold;
+      padding: 5px 20px;
+      background: #{$blue-dark-color};
+      border: none;
+      border-radius: 5px;
+      color: #{$white-color};
+      font-size: 11px;
     }
-    .table-detail {
-      width: calc( 70% - 5px );
+
+    .btn-primary:hover {
+      cursor: pointer;
+      background: #{$blue-deep-dark-color};
     }
   }
 }
